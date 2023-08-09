570341c3491aebf630444fef622c2ad9db564963
ayush <rajayush746@gmail.com>
2023-08-09 13:34:09 +0530

    changes

--- 7) sorting/MissingNumber.java
+++ 7) sorting/MissingNumber.java
@@ -1,13 +1,30 @@
 //https://leetcode.com/problems/missing-number/
-import java.util.*;
-public class MissingNumber {
-        public int missingNumber(int[] nums) {
-            Arrays.sort(nums);  // we can also use here sorting algorithms instead of this helper function
-            int count = 0;
-            for(int i=0;i<nums.length;i++){
-                if(nums[i] != count) return count;
-                count++;
-            }
-            return count;
+class Solution {
+    public int missingNumber(int[] nums) {
+        cyclicSort(nums);
+        int i=0;
+        while(i<nums.length){
+            if(nums[i]!=i) return i;
+            else i++;
         }
+        return nums.length;
+    }
+
+    public void cyclicSort(int[] nums){
+        int j=0;
+        while(j<nums.length){
+            int correctIndex = nums[j];
+            if(nums[j] < nums.length &&  nums[j] != nums[correctIndex]){ // if any nums[i] == nums.length, then we will igonre it so at the end it will automatically go to end
+                swap(nums, j, correctIndex);
+            }else{
+                j++;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int s, int e){
+        int temp = nums[s];
+        nums[s] = nums[e];
+        nums[e] = temp;
+    }
 }
