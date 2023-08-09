8f1948288c7535460299d4b1fa0df67ce8def768
ayush <rajayush746@gmail.com>
2023-08-09 18:44:15 +0530

    another one

--- 7) sorting/FindDuplicateNo.java
+++ 7) sorting/FindDuplicateNo.java
@@ -1,25 +1,25 @@
 //https://leetcode.com/problems/find-the-duplicate-number/
 public class FindDuplicateNo {
-        public int findDuplicate(int[] nums) {
-            // simple cyclic sort, and some logic to get answer
-            int i=0;
-            while(i<nums.length){
-                int correct = nums[i]-1;
-                if(nums[i]!=nums[correct]){
-                    swap(nums, i, correct);
-                }
-                else{i++;}
+    public int findDuplicate(int[] nums) {
+        // simple cyclic sort, and some logic to get answer
+        int i = 0;
+        int ans = 0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                swap(nums, i, correct);
             }
-
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]!=j+1) return nums[j];
+            else{
+                ans=nums[i]; // this will run when u will check after swapping that nums[correct] i.e. 3 == 3 means duplicate then else condition will run and will print that element
+                i++;
             }
-            return -1;
         }
+        return ans;
+    }
 
-        public void swap(int[] nums, int s, int e){
-            int temp = nums[s];
-            nums[s] = nums[e];
-            nums[e] = temp;
-        }
+    public void swap(int[] nums, int s, int e){
+        int temp = nums[s];
+        nums[s] = nums[e];
+        nums[e] = temp;
+    }
 }
