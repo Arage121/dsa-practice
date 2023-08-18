19a257b5b9cd03f524cae78f9f94ce9ee834d1e9
ayush <rajayush746@gmail.com>
2023-08-18 16:13:16 +0530

    more

--- 10) bitwise/NumberOfOneBits.java
+++ 10) bitwise/NumberOfOneBits.java
@@ -1,11 +1,11 @@
 //https://leetcode.com/problems/number-of-1-bits/
 public class NumberOfOneBits {
     public int hammingWeight(int n) {
-            int count = 0;
-            while(n!=0){
-                count += n&1;//check the least significant bit
-                n >>>= 1;
-            }
-            return count;
+        int count = 0;
+        while(n!=0){
+            if((n & 1) == 1) count++; // least significant bit, check 1 is there or not
+            n >>>= 1; // if not there it will remove and in it's place another left value comes and this goes on
         }
+        return count;
+    }
 }
