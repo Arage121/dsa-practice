da5f13472253585c886b82db8fe48d9c648e1a43
ayush <rajayush746@gmail.com>
2023-08-19 15:58:07 +0530

    updated

--- 9) recursion/NoOfStepsToReduceNoToZero.java
+++ 9) recursion/NoOfStepsToReduceNoToZero.java
@@ -1,10 +1,12 @@
 //https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
 public class NoOfStepsToReduceNoToZero {
         public int numberOfSteps(int num) {
-            if(num == 0) return 0;
-            if(num%2==0){
-                return numberOfSteps(num/2)+1;
-            }
-            return numberOfSteps(num-1)+1;
+            return helper(num, 0);
+        }
+
+        public int helper(int n, int steps){
+            if(n == 0) return steps;
+            if(n%2==0) return helper(n/2, steps+1);
+            return helper(n-1, steps+1);
         }
 }
