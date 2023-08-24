3d45adcb760ea23445647c1e03b9395f9086489c
Arage121 <rajayush746@gmail.com>
2023-08-24 21:55:37 +0530

    words

---
+++ 8-strings/ReverseWordsInaString.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/reverse-words-in-a-string/
+public class ReverseWordsInaString {
+    public String reverseWords(String s) {
+        StringBuilder ans=new StringBuilder();
+        for(int start = s.length()-1; start >=0 ; start--) {
+            if(s.charAt(start)==' '){
+                continue;
+            }
+            int end=start;
+            while(start>=0 && s.charAt(start)!=' '){
+                start--;
+            }
+            ans.append(s.substring(start+1,end+1)).append(" ");
+        }
+        return ans.toString().strip();
+    }
+}
