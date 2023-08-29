616d148fb061fd9708df8e5295a3f13cdeecba2d
Arage121 <rajayush746@gmail.com>
2023-08-29 22:09:34 +0530

    mod

--- 5-arrays/Pangram.java
+++ 5-arrays/Pangram.java
@@ -1,11 +1,14 @@
 //https://leetcode.com/problems/check-if-the-sentence-is-pangram/
 import java.util.*;
 public class Pangram {
-    public boolean checkIfPangram(String str) {
-        HashSet<Character> h = new HashSet<>();
-        for(int i=0;i<str.length();i++){
-            h.add(str.charAt(i));
+    public boolean checkIfPangram(String sentence) {
+        for (int i=97;i<=122;i++)
+        {
+            if(sentence.indexOf((char)i)<0)
+            {
+                return false;
+            }
         }
-        return h.size() == 26;
+        return true;
     }
 }
