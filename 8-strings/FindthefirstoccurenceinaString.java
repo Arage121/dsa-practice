e48b29cb9550ca69cd3004b0eedb668ac32616e2
Arage121 <rajayush746@gmail.com>
2023-09-04 23:18:01 +0530

    slide

---
+++ 8-strings/FindthefirstoccurenceinaString.java
@@ -0,0 +1,25 @@
+public class FindthefirstoccurenceinaString {
+        public int strStr(String haystack, String needle) { // sliding window approach, understand properly
+            int hLen = haystack.length();
+            int nLen = needle.length();
+            int nIndex = 0;
+            for(int i=0; i<hLen; i++){
+                // as long as the characters are equal, increment needleIndex
+                if(haystack.charAt(i)==needle.charAt(nIndex)){
+                    nIndex++;
+                }
+                else{
+                    // start from the next index of previous start index
+                    i=i-nIndex;
+                    // needle should start from index 0
+                    nIndex=0;
+                }
+                // check if needleIndex reached needle length
+                if(nIndex==nLen){
+                    // return the first index
+                    return i-nLen+1;
+                }
+            }
+            return -1;
+        }
+}
