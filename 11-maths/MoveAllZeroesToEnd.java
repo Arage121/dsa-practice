31ffb712e69fddd22192807fe53f0bdae2558b7f
Arage121 <rajayush746@gmail.com>
2023-10-31 09:52:26 +0530

    moveZeroesToEnd

---
+++ 11-maths/MoveAllZeroesToEnd.java
@@ -0,0 +1,16 @@
+public class MoveAllZeroesToEnd {
+    void pushZerosToEnd(int[] arr, int n) {
+        int nonZeroCount = 0;
+
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                arr[nonZeroCount++] = arr[i]; // here arr[nonZeroCount++] = arr[i] basically means same as arr[nonZeroCount] = arr[i], and in next line
+                                                                                                           // nonZeroCount++
+            }
+        }
+
+        while(nonZeroCount < n){
+            arr[nonZeroCount++] = 0;
+        }
+    }
+}
