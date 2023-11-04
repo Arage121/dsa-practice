414dbac179276386c059bd97a5b37d0f3443b169
Arage121 <rajayush746@gmail.com>
2023-11-04 17:59:48 +0530

    lastMomentBeforeAntsFall

---
+++ 11-maths/LastMomentBeforeAllAntsFallOutOfPlank.java
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/last-moment-before-all-ants-fall-out-of-a-plank/
+public class LastMomentBeforeAllAntsFallOutOfPlank {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int lastFallTime = 0;
+
+        // Determine the farthest position reached by ants moving to the left.
+        for (int leftPosition : left) {
+            lastFallTime = Math.max(lastFallTime, leftPosition);
+        }
+
+        // Determine the farthest position reached by ants moving to the right,
+        // considering they start from the right end of the plank (n - position).
+        for (int rightPosition : right) {
+            lastFallTime = Math.max(lastFallTime, n - rightPosition);
+        }
+
+        return lastFallTime;
+    }
+}
