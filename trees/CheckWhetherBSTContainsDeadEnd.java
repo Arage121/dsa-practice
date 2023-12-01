b540a359adde42f3f6689bb7d36e84fd00f566de
Arage121 <rajayush746@gmail.com>
2023-12-01 23:00:15 +0530

    deadEnd

---
+++ trees/CheckWhetherBSTContainsDeadEnd.java
@@ -0,0 +1,23 @@
+public class CheckWhetherBSTContainsDeadEnd {
+    class Node {
+        int data;
+        Node left, right;
+
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }
+    public static boolean isDeadEnd(Node root)
+    {
+        return isDeadUntil(root, 1, 10002); //range of value of nodes from the question
+    }
+
+    private static boolean isDeadUntil(Node root, int min, int max){
+        if(root == null) return false;
+        if(min == max) return true; // found deadend
+        boolean left = isDeadUntil(root.left, min, root.data-1);
+        boolean right = isDeadUntil(root.right, root.data+1, max);
+        return left || right;
+    }
+}
